b5aec418cf51b9aba8ad84bde39d395e6fe19c35
Davis, Vince <vincent.l.davis@united.com>
2023-04-23 11:09:40 -0500

    Initial Commit

---
+++ Live Pizza/Live_PizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Live_PizzaApp.swift
+//  Live Pizza
+//
+//  Created by Vince Davis on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Live_PizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
